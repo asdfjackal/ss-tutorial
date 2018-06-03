e028ad3e12ff197aaabb2078ee877ffc69164eee
Aleksandr White <aleksandr@asdfjackal.com>
2018-06-03 11:02:53 -0400

    Added pages for creation and listing of notes.

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -5,11 +5,13 @@ import Login from './containers/Login';
 import Signup from './containers/Signup';
 import NotFound from './containers/NotFound'
 import AppliedRoute from './components/AppliedRoute'
+import NewNote from './components/NewNote'
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
